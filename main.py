79d13e8b3aa63e08fff5225f6041fcb45b14a3a8
fantarics <66209368+fantarics@users.noreply.github.com>
2021-11-21 01:00:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from aiogram import Bot, Dispatcher, executor, types
 import time
 
-API_TOKEN = '1939859866:AAEcckHkODkDwIPVjQfUouXcv_sP85xiCQU'
-
+API_TOKEN = 'token'
+CHAT_ID = 'chat_id'
+ADMINS = [0,1,3]
+BANNED_WORDS = ['1','2','3','4']
 # Configure logging
 
 
@@ -10,7 +12,7 @@ API_TOKEN = '1939859866:AAEcckHkODkDwIPVjQfUouXcv_sP85xiCQU'
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-@dp.message_handler(commands=['mmm'], is_chat_admin=-1001459820272, user_id = [383387282, 385864579, 352220645])
+@dp.message_handler(commands=['mmm'], is_chat_admin=CHAT_ID, user_id = ADMINS)
 async def send_welcome(message: types.Message):
     if ' ' in message.text:
         print(' no ')
@@ -26,16 +28,16 @@ async def send_welcome(message: types.Message):
     #await message.answer(f'Всем привет, кроме кое-кого. Ему не привет на еще {round(mute_time)*60*60} секуууууунд')
     print(f'muted for {round(mute_time * 60 * 60)}')
     await bot.send_message(chat_id=chat_id, text=f'=0')
-    await bot.restrict_chat_member(chat_id=-1001459820272, user_id = who, until_date=time.time()+round(mute_time * 60 * 60))
+    await bot.restrict_chat_member(chat_id=chat_id, user_id = who, until_date=time.time()+round(mute_time * 60 * 60))
     print(f'muted for {round(mute_time)*60*60}')
 
 
-@dp.message_handler(text_contains=['zapravka'], is_chat_admin=-1001459820272)
+@dp.message_handler(text_contains=BANNED_WORDS, is_chat_admin=CHAT_ID)
 async def send_welcome(message: types.Message):
     user_id = message.from_user.id
     await message.delete()
     try:
-        await bot.restrict_chat_member(chat_id=-1001459820272, user_id=user_id,
+        await bot.restrict_chat_member(chat_id=CHAT_ID, user_id=user_id,
                                     until_date=time.time() + round(9999 * 60 * 60))
     except:
         await message.answer('Свой среди чужих бля пидораааас')
